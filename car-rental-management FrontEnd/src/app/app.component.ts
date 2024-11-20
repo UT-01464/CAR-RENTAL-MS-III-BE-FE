3f6783396c7d5be4db7d8771107cbdc548145c5d
ThenuSathees <thenujasatheeskumar@gmail.com>
2024-11-21 03:49:00 +0530

    LandingPage

--- car-rental-management FrontEnd/src/app/app.component.ts
+++ car-rental-management FrontEnd/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './LandingPage/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
     RouterModule,
-    CommonModule],
+    CommonModule,
+  NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
